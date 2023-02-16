9538ddef90eb148730c4eed88ae79dfeac5043fc
phuong-cat.ngo@sjsu.edu <44314126+catcecilia@users.noreply.github.com>
2023-02-16 17:58:56 -0500

    Replaced innerhtml with appending child

--- domListExercise/script.js
+++ domListExercise/script.js
@@ -1,7 +1,7 @@
 var button = document.getElementById("enter");
-var delButton = document.getElementsByClassName("del");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var items = ul.getElementsByTagName("li");
 
 function inputLength() {
 	return input.value.length;
@@ -10,7 +10,7 @@ function inputLength() {
 function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
-	li.innerHTML += "<button class='del'>x</button>";
+	li.innerHTML += "<button>x</button>";
 	ul.appendChild(li);
 	input.value = "";
 }
@@ -27,17 +27,28 @@ function addListAfterKeypress(event) {
 	}
 }
 
+//For every li in ul, create a button with text "x" and append it to li[i]
+for (var i = 0; i < items.length; i++){
+	var button = document.createElement('button');
+	button.appendChild(document.createTextNode("x"));
+	items[i].appendChild(button);
+}
+
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
 ul.addEventListener('click', function(e){
+	//when clicking at this event in the element ul, check to see if it is a button, if so, delete the parent child of button (li) thereby removing it
 	if (e.target.tagName == "BUTTON"){
 		parent = e.target.parentElement;
 		parent.remove();
 	}
+	//if it is li, toggle class name "done" instead
 	else if (e.target.tagName =="LI"){
 		var classes = e.target.classList;
 		classes.toggle("done");
 	}
+	//all else, do nothing
 });
